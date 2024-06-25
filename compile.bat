0d2fac90e433bcae82fce2b0bcbe730564cc6186
Stefan <stefan.zdra@gmail.com>
2024-06-25 13:50:19 +1200

    🐛 Fix filenames in build script

--- compile.bat
+++ compile.bat
@@ -4,9 +4,9 @@ setlocal
 rem Script for compiling the project into a ZIP file for distribution
 
 set files=pack.mcmeta LICENSE MANUAL.md README.md data/
-7z a -tzip output.zip %files%
+7z a -tzip better_carpets.zip %files%
 
 set files_to_remove=data/bc/function/template data/bc/function/generate_carpets.py data/bc/predicate/generate_predicates.py data/bc/function/countdown/generate_progress_bar.py
-7z d better_carpts.zip %files_to_remove%
+7z d better_carpets.zip %files_to_remove%
 
 endlocal
