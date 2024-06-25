af401ae7c99ccefa4bddf1a0a618dd36b7522011
Stefan <stefan.zdra@gmail.com>
2024-06-25 13:45:55 +1200

    👷 Added compile script Excludes website files and python generator scripts from releases automatically

---
+++ compile.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+
+rem Script for compiling the project into a ZIP file for distribution
+
+set files=pack.mcmeta LICENSE MANUAL.md README.md data/
+7z a -tzip output.zip %files%
+
+set files_to_remove=data/bc/function/template data/bc/function/generate_carpets.py data/bc/predicate/generate_predicates.py data/bc/function/countdown/generate_progress_bar.py
+7z d better_carpts.zip %files_to_remove%
+
+endlocal
